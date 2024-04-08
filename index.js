d5378dceb8defa8d11bd413092f18a1b448482ef
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-04-08 22:57:49 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+  };
+  
+  let mondayWork = function (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  /*
+  ! INFO
+  
+  IIFE: Immediately-Invoked Function Expression
+  
+  */
+  
+  // TODO: Visual flair function.
+  function wrapAdjective(flair = "*"){
+    // It should return a function.
+    // Properties of the inner function:-
+      // TODO: Take a single parameter that should default to "special".
+      return function (arg = "special"){
+        // TODO: Return a String of the form "You are ...!".
+        return `You are ${flair}${arg}${flair}!`;
+      };
+  };
+  
+  /*
+  ? Example of the total call:-
+  
+  * wrapAdjective("%")("a dedicated programmer");
+  
+  */
